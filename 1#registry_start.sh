b0a0a6eff31cf23e8739e5b8a43529f15d59228d
zfrhv <53610738+zfrhv@users.noreply.github.com>
2022-05-24 11:53:34 +0300

    Update 1#registry_start.sh

--- 1#registry_start.sh
+++ 1#registry_start.sh
@@ -1,5 +1,4 @@
 source 0#config.sh
 rm -rf ${registry_data}/*
-podman stop ${registry_name}
-podman rm ${registry_name}
-podman run --name ${registry_name} -d -p ${registry_port}:5000 -v ${registry_data}:/var/lib/registry:z docker.io/library/registry:2
+podman rm -f tmp-registry
+podman run --name tmp-registry -d -p ${registry_port}:5000 -v ${registry_data}:/var/lib/registry:z docker.io/library/registry:2
