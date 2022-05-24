3ca2f3b07a571288cd133326fc0ea360c0c8475a
root <root@deploy.j0hnlitzke4effikwyzyjmvmae.bx.internal.cloudapp.net>
2022-05-24 10:45:26 +0000

    a

--- 0#config.sh
+++ 0#config.sh
@@ -5,16 +5,13 @@ registry_name="localhost"
 registry_port="5013"
 registry_data="/var/registry/data51"
 #operators_list="cic-operator,elasticsearch-eck-operator-certified"
-operators_list="advanced-cluster-management"
-image_name="operator-index/advanced-cluster-management:06-03-2022"
-src_image="registry.redhat.io/redhat/redhat-operator-index:v4.8"
+operators_list="prometheus"
+image_name="operator-index/prometheus:24-05-2022"
+src_image="quay.io/operatorhubio/catalog:latest"
 REG_CREDS=$XDG_RUNTIME_DIR/containers/auth.json
 REGISTRY_AUTH_FILE=$XDG_CONFIG_HOME/containers/auth.json
 
-#registry.redhat.io/redhat/certified-operator-index:v4.8
-#registry.redhat.io/redhat/community-operator-index:v4.8
-#registry.redhat.io/redhat/redhat-marketplace-index:v4.8
-#registry.redhat.io/redhat/redhat-operator-index:v4.8
+#quay.io/operatorhubio/catalog:latest
 
 
 folder_name="$(echo "$image_name" | grep -Po '(?<=/).+(?=:)')"
--- 2#prune.sh
+++ 2#prune.sh
@@ -3,9 +3,16 @@ source 0#config.sh
 mkdir to-take-$folder_name
 cd to-take-$folder_name
 
-echo "please login to registry.redhat.io"
-podman login registry.redhat.io
-opm index prune -f ${src_image} -p ${operators_list} -t localhost:${registry_port}/${image_name}
+mkdir -p pruned-catalog/configs
 
+opm render $src_image | jq 'select( .package == "'$operators_list'" or .name == "'$operators_list'")' > pruned-catalog/configs/index.json
+
+opm alpha generate dockerfile pruned-catalog/configs && cd pruned-catalog/
+
+podman build -t localhost:${registry_port}/${image_name} -f configs.Dockerfile .
+
+cd ..
 podman save localhost:${registry_port}/${image_name} -o index.tar
 podman push localhost:${registry_port}/${image_name}
+
+rm -rf pruned-catalog
